2c9deb48f9205273b2c6ff0a168984ff72ed7269
Konrad <konradklimczak93@gmail.com>
2017-12-08 20:16:45 +0100

    React router added and some files.

--- src/Page/Main.tsx
+++ src/Page/Main.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import './App.css';
 
-class App extends React.Component {
+class Main extends React.Component {
   render() {
     return (
       <div>
@@ -24,4 +23,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default Main;
