26f07d9e2eaba1873ca079b7007d659ab08b4b09
Konrad Klimczak <konrad.klimczak@vistex.com>
2018-01-09 11:53:43 +0100

    Header styles started.

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-import './Header.css';
+import "./Header.css";
 
 export default () => (
   <header className="lele--header">
-    <i className="fa fa-bars" aria-hidden="true" />
-    Language Exchange
+    <div className="burger-icon">
+      <i className="fa fa-bars" aria-hidden="true" />
+    </div>
+    <h1>Language Exchange</h1>
     <nav>
       <Link to="/">Home</Link>
       <Link to="/create-lesson">Create Lesson</Link>
