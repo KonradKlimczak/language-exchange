fe5ce799438f50243bb051cac08050b117007375
Konrad Klimczak <konrad.klimczak@vistex.com>
2018-01-09 10:58:41 +0100

    font awesome added to code.

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-import './Header.css';
+import "./Header.css";
 
 export default () => (
   <header className="lele--header">
+    <i className="fa fa-bars" aria-hidden="true" />
     Language Exchange
     <nav>
       <Link to="/">Home</Link>
