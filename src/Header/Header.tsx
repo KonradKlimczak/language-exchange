380dfbba2fe451931fde72f7b5c7eb333667bf84
Konrad Klimczak <konrad.klimczak@vistex.com>
2018-01-09 10:59:44 +0100

    Lint fix.

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./Header.css";
+import './Header.css';
 
 export default () => (
   <header className="lele--header">
