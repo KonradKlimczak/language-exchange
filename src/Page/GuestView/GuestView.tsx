af69cbc8bb4cfa4b803752e2684af84c1513e9de
Konrad Klimczak <konradklimczak93@gmail.com>
2018-03-04 11:53:31 +0100

    State change.

--- src/Page/GuestView/GuestView.tsx
+++ src/Page/GuestView/GuestView.tsx
@@ -1,39 +1,22 @@
 import * as React from 'react';
 import { TextField } from '../../Components/TextField/TextField';
 
-enum FiledKind {
-  Empty = 'Empty',
-  Valid = 'Valid',
-  Invaild = 'Invalid'
-}
-
-type Field<T> = {
-  kind: FiledKind.Empty,
-} | {
-    kind: FiledKind.Valid,
-    value: T,
-  } | {
-    kind: FiledKind.Invaild,
-    value: T,
-    error: string
-  };
-
 interface GuestViewProps { }
 interface GuestViewState {
-  username: Field<string>;
-  email: Field<string>;
-  password: Field<string>;
-  passwordAgain: Field<string>;
+  username: string;
+  email: string;
+  password: string;
+  passwordAgain: string;
 }
 
 export default class GuestView extends React.Component<GuestViewProps, GuestViewState> {
   constructor(props: GuestViewProps) {
     super(props);
     this.state = {
-      username: { kind: FiledKind.Empty },
-      email: { kind: FiledKind.Empty },
-      password: { kind: FiledKind.Empty },
-      passwordAgain: { kind: FiledKind.Empty },
+      username: '',
+      email: '',
+      password: '',
+      passwordAgain: '',
     };
   }
   render() {
